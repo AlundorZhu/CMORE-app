d0de6a8ccddca29449de7b95b731221f4ae2a832
Ziqiang (Joe) Zhu <ziqzhu@ucdavis.edu>
2026-02-19 16:14:48 -0800

    added with last fix

--- CMORE/Screens/ProcessImportedVideo/VideoProcessingViewModel.swift
+++ CMORE/Screens/ProcessImportedVideo/VideoProcessingViewModel.swift
@@ -35,7 +35,7 @@ class VideoProcessingViewModel: ObservableObject {
     init() {
         self.frameProcessor = FrameProcessor(
             onCross: { /* no sound during video processing */ },
-            perFrame: { [weak self] result in
+            partialResult: { [weak self] result in
                 guard let self else { return }
 
                 Task { @MainActor in
